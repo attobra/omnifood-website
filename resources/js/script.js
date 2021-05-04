ac37f7c7635df329a2f676ff37a734af77b418e8
attobra <nora.attobra@gmail.com>
2021-05-04 19:27:51 -0400

    add final version

--- resources/js/script.js
+++ resources/js/script.js
@@ -74,7 +74,54 @@ $(document).ready(function () {
     });
 
   /* Animations  on scroll */
-  $(".js--wp-1").waypoint(function (direction) {
-    $(".js--wp-1").addClass("animated fadeIn");
+  $(".js--wp-1").waypoint(
+    function (direction) {
+      $(".js--wp-1").addClass("animate__animated animate__fadeIn");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  $(".js--wp-2").waypoint(
+    function (direction) {
+      $(".js--wp-2").addClass("animate__animated animate__fadeInUp");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  $(".js--wp-3").waypoint(
+    function (direction) {
+      $(".js--wp-3").addClass("animate__animated animate__fadeIn");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  $(".js--wp-4").waypoint(
+    function (direction) {
+      $(".js--wp-4").addClass("animate__animated animate__pulse");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  /* hamburger menu on mobile */
+  $(".js--nav-icon").click(function () {
+    var nav = $(".js--main-nav");
+    var icon = $(".js--nav-icon ion-icon");
+    nav.slideToggle(200);
+
+    if (icon.hasClass("menu")) {
+      icon.addClass("close");
+      icon.removeClass("menu");
+    } else {
+      icon.addClass("menu");
+      icon.removeClass("close");
+    }
   });
 });
